0760608e44cc27b4a1c6a07b30d7b91cb7a32621
Anshuman Sarkar <anshumansarkar700@gmail.com>
2026-09-25 13:34:48 +0530

    Update sort.c

--- sort.c
+++ sort.c
@@ -14,7 +14,7 @@ void bubbleSort(int arr[], int n){
     bool swapped;
     for (i = 0; i < n - 1) {
         swapped = false;
-        for (j = 0; j < n - i - 1) {
+        for (j = 0; j < ) {
             if (arr[j] > arr[j + 1]) {
                 swap(&arr[j], &arr[j + 1]);
                 swapped = true;
